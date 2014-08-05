697e9ad4606516e6a4bcc42e9b0adbfcc4a23471
joerg <joerg.fischer@teradata.com>
2014-08-05 23:34:25 +0200

    Chapter 3 exercises and tests for tree functions

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,47 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size(tree: Tree[Any]): Int =
+    tree match {
+      case Branch(left, right) => size(left) + 1 + size(right)
+      case Leaf(_) => 1
+    }
 
+  def maximum(tree: Tree[Int]): Int =
+    tree match {
+      case Branch(left, right) =>  maximum(left) max maximum(right)
+      case Leaf(value) => value
+    }
 
+  def depth(tree: Tree[Any]): Int = {
+    def traverse(t: Tree[Any], acc: Int): Int = t match {
+      case Branch(left, right) => traverse(left, acc + 1) max traverse(right, acc + 1)
+      case Leaf(_) => acc
+    }
+    traverse(tree, 1)
+  }
 
+  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] =
+    tree match {
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+      case Leaf(value) => Leaf(f(value))
+    }
+
+  def fold[A,B,C](tree: Tree[A])(f: A => B)(g: (B, B) => B): B =
+    tree match {
+      case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+      case Leaf(value) => f(value)
+  	}
+
+  def sizeViaFold(tree: Tree[Any]): Int =
+    fold(tree)(v => 1)(_ + 1 + _)
+
+  def maximumViaFold(tree: Tree[Int]): Int =
+    fold(tree)(v => v)(_ max _)
+
+  def depthViaFold(tree: Tree[Any]): Int =
+    fold(tree)(v => 1)((l, r) => 1 + (l max r))
+
+  def mapViaFold[A,B](tree: Tree[A])(f: A => B): Tree[B] =
+    fold(tree)(v => Leaf(f(v)): Tree[B])(Branch(_, _))
 }
---
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -0,0 +1,95 @@
+package fpinscala.datastructures
+
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import fpinscala.datastructures.Tree._
+
+trait SharedTests {
+  self: FlatSpec =>
+
+  def sizeFunction(size: Tree[Any] => Int) {
+    it should "return the total number of nodes in the tree" in {
+      assert(size(Leaf(1)) === 1)
+      assert(size(Branch(Leaf(1), Leaf(2))) === 3)
+      assert(size(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) === 5)
+    }
+  }
+
+  def maximumFunction(maximum: Tree[Int] => Int) {
+    it should "return the maximum value from the tree" in {
+      assert(maximum(Leaf(1)) === 1)
+      assert(maximum(Branch(Leaf(3), Leaf(1))) === 3)
+      assert(maximum(Branch(Leaf(5), Branch(Leaf(7), Leaf(2)))) === 7)
+    }
+  }
+
+  def depthFunction(depth: Tree[Any] => Int) {
+    it should "return the maximum depth of the tree" in {
+      assert(depth(Leaf(1)) === 1)
+      assert(depth(Branch(Leaf(1), Leaf(1))) === 2)
+      assert(depth(Branch(Leaf(1), Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))) === 4)
+    }
+  }
+
+//  def mapFunction[A,B](map: Tree[A] => (A => B) => Tree[B]) {
+//    it should "apply the function to all elements of the tree" in {
+//      def double(value: Int): Int = 2 * value
+//      assert(map(Leaf(1))(double) === Leaf(2))
+//      assert(map(Branch(Leaf(1), Leaf(2)))(double) === Branch(Leaf(2), Leaf(4)))
+//    }
+//  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class TreeSpec extends FlatSpec with SharedTests {
+
+  behavior of "size"
+
+  it should behave like sizeFunction(size)
+
+  behavior of "maximum"
+
+  it should behave like maximumFunction(maximum)
+
+  behavior of "depth"
+
+  it should behave like depthFunction(depth)
+
+  behavior of "map"
+
+//  it should behave like mapFunction(map)
+  it should "apply the function to all elements of the tree" in {
+    def double(value: Int): Int = 2 * value
+    assert(map(Leaf(1))(double) === Leaf(2))
+    assert(map(Branch(Leaf(1), Leaf(2)))(double) === Branch(Leaf(2), Leaf(4)))
+  }
+
+  behavior of "fold"
+
+  it should "perform a pre-order traversal of the tree" in {
+    val tree = Branch(Leaf("A"), Branch(Leaf("B"), Leaf("C")))
+    assert(fold(tree)(v => v)(_ + _) === "ABC")
+  }
+
+  behavior of "sizeViaFold"
+
+  it should behave like sizeFunction(sizeViaFold)
+
+  behavior of "maximumViaFold"
+
+  it should behave like maximumFunction(maximumViaFold)
+
+  behavior of "depthViaFold"
+
+  it should behave like depthFunction(depthViaFold)
+
+  behavior of "mapViaFold"
+
+//  it should behave like mapFunction(mapViaFold)
+  it should "apply the function to all elements of the tree" in {
+    def double(value: Int): Int = 2 * value
+    assert(map(Leaf(1))(double) === Leaf(2))
+    assert(map(Branch(Leaf(1), Leaf(2)))(double) === Branch(Leaf(2), Leaf(4)))
+  }
+}
